9b50238552f8f7c700ba30491a15b5007ad2eceb
Vineet Vora <Vineet@wirelessprv-10-194-210-137.near.illinois.edu>
2017-11-15 17:26:30 -0600

    Fuck Sarikonda

--- UserPortal/result.php
+++ UserPortal/result.php
@@ -20,7 +20,6 @@
     <link href="css/elements.css" rel="stylesheet">
 
     <script src="js/my_js.js"></script>
-    <title>Table with database</title>
   </head>
 
   <body>
@@ -44,37 +43,37 @@
   			<th>Designation</th>
   		</tr>
   		<?php
-$conn = mysql_connect("localhost", "root", "password");
-mysql_select_db("VineyardWinesDB", $conn);
+		$conn = mysql_connect("localhost", "root", "password");
+		mysql_select_db("VineyardWinesDB", $conn);
 
 
-if($_REQUEST['submit']){
-$name = $_POST['name'];
-if(empty($name)){
-	$make = '<h4>You must type a word to search!</h4>';
-}else{
-	$make = '<h4>No match found!</h4>';
-	$sele = "SELECT * FROM Wines WHERE variety LIKE '%$name%'";
-	$result = mysql_query($sele);
-	if(!$result) {
-		print("Bad Query");
-	}
-	if(mysql_num_rows($result) > 0){
-		$i = 0;
-		while($row = mysql_fetch_assoc($result) and $i < 50){
-		echo "<tr><td>". $row['variety'] ."</td><td>". $row['designation'] ."</td></tr>"
-		$i = $i + 1;
+		if($_REQUEST['submit']){
+		$name = $_POST['name'];
+		if(empty($name)){
+			$make = '<h4>You must type a word to search!</h4>';
+		}else{
+			$make = '<h4>No match found!</h4>';
+			$sele = "SELECT * FROM Wines WHERE variety LIKE '%$name%'";
+			$result = mysql_query($sele);
+			if(!$result) {
+				print("Bad Query");
+			}
+			if(mysql_num_rows($result) > 0){
+				$i = 0;
+				while($row = mysql_fetch_assoc($result) and $i < 50){
+				echo "<tr><td>". $row['variety'] ."</td><td>". $row['designation'] ."</td></tr>"
+				$i = $i + 1;
 
-	}
-}else{
-echo'<h2> Search Result</h2>';
-print ($make);
-}
-mysql_free_result($result);
-mysql_close($conn);
-}
-}
-?>
+			}
+		}else{
+		echo'<h2> Search Result</h2>';
+		print ($make);
+		}
+		mysql_free_result($result);
+		mysql_close($conn);
+		}
+		}
+		?>
   	</table>
   </body>
 
