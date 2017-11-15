a7bbd461bac5685f940c3dc274253dd4dd70f056
root <root@fa17-cs411-01.cs.illinois.edu>
2017-11-15 06:17:08 -0600

    gainz

--- UserPortal/result.php
+++ UserPortal/result.php
@@ -9,10 +9,13 @@ if(empty($name)){
 	$make = '<h4>You must type a word to search!</h4>';
 }else{
 	$make = '<h4>No match found!</h4>';
-	$sele = "SELECT * FROM 'Wines' WHERE 'variety' LIKE '%$name%'";
+	$sele = "SELECT * FROM Wines WHERE variety LIKE '%$name%'";
 	$result = mysql_query($sele);
+	if(!$result) {
+		print("Bad Query");
+	}
 
-	if($mak = mysql_num_rows($result) > 0){
+	if(mysql_num_rows($result) > 0){
 		while($row = mysql_fetch_assoc($result)){
 		echo '<h4> Variety					: '.$row['variety'];
 		echo '<br> Designation						: '.$row['designation'];
