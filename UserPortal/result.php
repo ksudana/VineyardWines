83f071ce34dba935c397cb1eab03da7d297975a4
Akhil Reddy <sarikon2@illinois.edu>
2017-11-15 06:03:21 -0600

    edited result

--- UserPortal/result.php
+++ UserPortal/result.php
@@ -9,7 +9,7 @@ if(empty($name)){
 	$make = '<h4>You must type a word to search!</h4>';
 }else{
 	$make = '<h4>No match found!</h4>';
-	$sele = "SELECT * FROM `Wines` WHERE name LIKE '%$name%'";
+	$sele = "SELECT * FROM Wines WHERE name LIKE '%$name%'";
 	$result = mysql_query($sele);
 
 	if($mak = mysql_num_rows($result) > 0){
