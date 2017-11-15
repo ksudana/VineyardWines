0b430fb918bb75d78bb3553d7e37fddb9eef9a11
Karthik Sudana <ksudana@wirelessprv-10-194-23-82.near.illinois.edu>
2017-11-15 17:00:09 -0600

    ayy

--- UserPortal/nav.php
+++ UserPortal/nav.php
@@ -1,7 +1,7 @@
 <html>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a href="../index.php"><img src="Images/logo.png" style="width:25%"/></a>
+        <a href="../index.php"><img src="../Images/logo.png" style="width:25%"/></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
