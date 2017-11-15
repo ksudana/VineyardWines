195a181727f611c69424583533ff2405ca1db619
Akhil Reddy <sarikon2@illinois.edu>
2017-11-15 15:38:53 -0600

    edited 1

--- UserPortal/search.php
+++ UserPortal/search.php
@@ -1,9 +1,50 @@
 <html>
 <title> PHP MYSQL - Search</title>
 <head>
+
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+
+	    <title>VineyardWines</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link href="../Bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom styles for this template -->
+	    <link href="../Bootstrap/css/shop-item.css" rel="stylesheet">
+	    <link href="https://fonts.googleapis.com/css?family=Spectral+SC" rel="stylesheet">
 </head>
 
 <body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="#">Start Bootstrap</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Fuck
+							<span class="sr-only">(current)</span>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Akhil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Sarikonda</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="../index.html">Logout</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
 <form action="result.php" method="POST">
 <center><h3>Search Database</h3></center>
 <center><table>
@@ -14,6 +55,16 @@
 </tr>
 </table></center>
 </form>
+<footer class="py-1 bg-dark">
+	<div class="container">
+		<p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+	</div>
+	<!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
