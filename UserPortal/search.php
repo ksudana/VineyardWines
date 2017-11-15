658d8ffadeb92eaf0b33afc9a372a7310775fc33
Akhil Reddy <sarikon2@illinois.edu>
2017-11-15 16:00:36 -0600

    edit 4

--- UserPortal/search.php
+++ UserPortal/search.php
@@ -48,16 +48,6 @@
 <form action="result.php" method="POST">
 <titlec><center><h3>Discover New Tastes</h3></center></titlec>
 <center><table>
-<tr>
-	<div class="wrap">
-	   <div class="search">
-	      <input type="text" class="searchTerm" placeholder="What are you looking for?">
-	      <button type="submit" class="searchButton">
-	        <i class="fa fa-search"></i>
-	     </button>
-	   </div>
-	</div>
-</tr>
 
 <tr>
 	<td><input type="text" name="name" size="100"></td>
