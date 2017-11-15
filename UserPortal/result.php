5238fd828acb3a7872fa3e2e706cec0fb5b70c87
Vineet Vora <Vineet@wirelessprv-10-194-210-137.near.illinois.edu>
2017-11-15 16:38:11 -0600

    Fuck Sarikonda

--- UserPortal/result.php
+++ UserPortal/result.php
@@ -30,6 +30,7 @@ mysql_free_result($result);
 mysql_close($conn);
 }
 }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -50,5 +51,24 @@ mysql_close($conn);
 
     <script src="js/my_js.js"></script>
   </head>
-  </html>
-?>
+
+  <body>
+
+    <!-- Navigation -->
+    
+
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
