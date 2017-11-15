5bf58308cac63ad9e02ca4d5a4cb4e61f47dc39d
Akhil Reddy <sarikon2@illinois.edu>
2017-11-15 16:27:06 -0600

    fuck vineet vora

--- UserPortal/result.php
+++ UserPortal/result.php
@@ -1,3 +1,26 @@
+<html>
+<title> Discover</title>
+<head>
+
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+
+	    <title>VineyardWines</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link href="../Bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom styles for this template -->
+	    <link href="../Bootstrap/css/shop-item.css" rel="stylesheet">
+	    <link href="https://fonts.googleapis.com/css?family=Spectral+SC" rel="stylesheet">
+</head>
+
+<body>
+
+
+
 <?php
 include_once 'nav.php';
 $conn = mysql_connect("localhost", "root", "password");
@@ -31,3 +54,9 @@ mysql_close($conn);
 }
 }
 ?>
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
