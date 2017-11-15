54246d3d446711f9dca3a8e7dc3f6bc6e3f4d745
Karthik Sudana <ksudana@wirelessprv-10-195-229-118.near.illinois.edu>
2017-11-15 05:14:58 -0600

    pls

--- new_account.php
+++ new_account.php
@@ -18,7 +18,9 @@
     $sex = $_POST["sex"];
     $query = "INSERT INTO Users (name, username, password, age, country, sex) VALUES ('$name', '$username', '$password', '$age', '$country', '$sex')";
     $result = $con->query($query);
-    echo $result;
+    $query1 = "SELECT * FROM Wines WHERE wid=1"
+    $result1 = $con->query($query);
+    echo $result1;
     echo $name;
     echo "Created Account!";
   }
