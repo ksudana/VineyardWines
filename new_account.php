65c4747bda4bbe702f946949777bfb02276dcdfd
Karthik Sudana <ksudana@wirelessprv-10-195-229-118.near.illinois.edu>
2017-11-15 05:53:48 -0600

    pls

--- new_account.php
+++ new_account.php
@@ -16,11 +16,11 @@
     $age = $_POST["age"];
     $country = $_POST["country"];
     $sex = $_POST["sex"];
-    $query = "INSERT INTO Users (name, username, password, age, country, sex) VALUES ('Karthik', 'ksudana', 'pass', 20, 'US', 'Male')";
+    $query = "INSERT INTO Users (name, username, password, age, country, sex) VALUES ('$name', '$username', '$password', $age, '$country', '$sex')";
     $result = mysqli_query($con, $query);
     echo "Created Account!";
   }
-  mysqli_close($con);
+    mysqli_close($db);
   ?>
 
   <form action="index.html">
