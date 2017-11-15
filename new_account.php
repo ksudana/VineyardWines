f03711d4893234a0cbbcf49b8be06b25bdc22023
Karthik Sudana <ksudana@wirelessprv-10-195-229-118.near.illinois.edu>
2017-11-15 04:35:52 -0600

    bigdaddy

---
+++ new_account.php
@@ -0,0 +1,28 @@
+<html>
+  <?php
+  $hn = "localhost";
+  $un = "root";
+  $pw = "password";
+  $db = "VineyardWinesDB";
+
+  $con = mysqli_connect($hn, $un, $pw, $db);
+  if(!$con) {
+    echo "Connection failed!";
+  }
+  else {
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $age = $_POST["age"];
+    $country = $_POST["country"];
+    $sex = $_POST["sex"];
+    $query = "INSERT INTO Users(name, username, password, age, country, sex) VALUES ('$name', '$username', '$password', '$age', '$country', '$sex')";
+    $result = $con->query($query);
+    echo "Created Account!";
+  }
+  ?>
+
+  <form action="index.html">
+    <input type="submit" value="Back">
+  </form>
+</html>
