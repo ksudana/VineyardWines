f0a7baa1bed8263bbe005daeb61f6054c235546b
Akhil Reddy <sarikon2@illinois.edu>
2017-11-15 06:01:05 -0600

    edited files

---
+++ UserPortal/result.php
@@ -0,0 +1,29 @@
+<?php
+$conn = mysql_connect("localhost", "root", "password");
+mysql_select_db("VineyardWinesDB", $conn);
+
+
+if($_REQUEST['submit']){
+$name = $_POST['name'];
+if(empty($name)){
+	$make = '<h4>You must type a word to search!</h4>';
+}else{
+	$make = '<h4>No match found!</h4>';
+	$sele = "SELECT * FROM `Wines` WHERE name LIKE '%$name%'";
+	$result = mysql_query($sele);
+
+	if($mak = mysql_num_rows($result) > 0){
+		while($row = mysql_fetch_assoc($result)){
+		echo '<h4> Variety					: '.$row['variety'];
+		echo '<br> Designation						: '.$row['designation'];
+		echo '</h4>';
+	}
+}else{
+echo'<h2> Search Result</h2>';
+print ($make);
+}
+mysql_free_result($result);
+mysql_close($conn);
+}
+}
+?>
--- UserPortal/search.php
+++ UserPortal/search.php
@@ -1,30 +1,19 @@
-<?php
-//Step1
- $db = mysqli_connect('localhost','root','password','VineyardWinesDB')
- or die('Error connecting to MySQL server.');
-?>
-
 <html>
- <head>
- </head>
- <body>
- <h1>PHP connect to MySQL</h1>
+<title> PHP MYSQL - Search</title>
+<head>
+</head>
 
-<?php
-//Step2
-$query = "SELECT * FROM `Wines`";
-mysqli_query($db, $query) or die('Error querying database.');
-
-//Step3
-$result = mysqli_query($db, $query);
-$row = mysqli_fetch_array($result);
-
-while ($row = mysqli_fetch_array($result)) {
- echo $row['variety'] . ' ' . $row['designation'] . ': ' . $row['points'] . ' ' . $row['price'] .'<br />';
-}
-//Step 4
-mysqli_close($db);
-?>
-
+<body>
+<form action="result.php" method="POST">
+<center><h3>Search Database</h3></center>
+<center><table>
+<tr>
+	<td>Search</td>
+	<td><input type="text" name="name" size="100"></td>
+	<td><input type="submit" name="submit"></td>
+</tr>
+</table></center>
+</form>
 </body>
+
 </html>
