837ea8d8c56a414a2877814affe2b8b9f6e6a259
Vineet Vora <Vineet@wirelessprv-10-194-210-137.near.illinois.edu>
2017-11-15 17:04:23 -0600

    Fuck Sarikonda

--- UserPortal/result.php
+++ UserPortal/result.php
@@ -1,38 +1,3 @@
-<?php
-    include_once 'nav.php';
-$conn = mysql_connect("localhost", "root", "password");
-mysql_select_db("VineyardWinesDB", $conn);
-
-
-if($_REQUEST['submit']){
-$name = $_POST['name'];
-if(empty($name)){
-	$make = '<h4>You must type a word to search!</h4>';
-}else{
-	$make = '<h4>No match found!</h4>';
-	$sele = "SELECT * FROM Wines WHERE variety LIKE '%$name%'";
-	$result = mysql_query($sele);
-	if(!$result) {
-		print("Bad Query");
-	}
-	if(mysql_num_rows($result) > 0){
-		$i = 0;
-		while($row = mysql_fetch_assoc($result) and $i < 50){
-		echo '<h4> Variety					: '.$row['variety'];
-		echo '<br> Designation						: '.$row['designation'];
-		echo '</h4>';
-		$i = $i + 1;
-
-	}
-}else{
-echo'<h2> Search Result</h2>';
-print ($make);
-}
-mysql_free_result($result);
-mysql_close($conn);
-}
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -74,3 +39,38 @@ mysql_close($conn);
   </body>
 
 </html>
+<?php
+    include_once 'nav.php';
+$conn = mysql_connect("localhost", "root", "password");
+mysql_select_db("VineyardWinesDB", $conn);
+
+
+if($_REQUEST['submit']){
+$name = $_POST['name'];
+if(empty($name)){
+	$make = '<h4>You must type a word to search!</h4>';
+}else{
+	$make = '<h4>No match found!</h4>';
+	$sele = "SELECT * FROM Wines WHERE variety LIKE '%$name%'";
+	$result = mysql_query($sele);
+	if(!$result) {
+		print("Bad Query");
+	}
+	if(mysql_num_rows($result) > 0){
+		$i = 0;
+		while($row = mysql_fetch_assoc($result) and $i < 50){
+		echo '<h4> Variety					: '.$row['variety'];
+		echo '<br> Designation						: '.$row['designation'];
+		echo '</h4>';
+		$i = $i + 1;
+
+	}
+}else{
+echo'<h2> Search Result</h2>';
+print ($make);
+}
+mysql_free_result($result);
+mysql_close($conn);
+}
+}
+?>
