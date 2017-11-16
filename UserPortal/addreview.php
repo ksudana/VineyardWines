f4ff83fdfd8aaf442440602e849d3847e94f6749
Karthik Sudana <ksudana@wirelessprv-10-194-23-82.near.illinois.edu>
2017-11-16 00:19:15 -0600

    adding reviews

---
+++ UserPortal/addreview.php
@@ -0,0 +1,34 @@
+<?php 
+    session_start();
+?>
+<html>
+  <?php
+  $hn = "localhost";
+  $un = "root";
+  $pw = "password";
+  $db = "VineyardWinesDB";
+
+  $con = mysqli_connect($hn, $un, $pw, $db);
+  if(!$con) {
+    echo "Connection failed!";
+  }
+  else {
+    $uid = $_SESSION['uid'];
+    $wid = $_GET["wid"];
+    $title = $_POST["name"];
+    $mysqldate = date( 'Y-m-d H:i:s', $phpdate );
+    $rating = $_POST["rating"];
+    $recommend = $_POST["Reccomend"];
+    $recommend = ($recommend == "Yes") ? 1 : 0;
+    $review = $_POST["message"];
+    $query = "INSERT INTO Reviews (uid, wid, title, content, date, rating, recommend) VALUES ('$uid', '$wid', '$title', '$review'', '$mysqldate', $rating', '$recommend')";
+    $result = mysqli_query($con, $query);
+    echo "Created Account!";
+  }
+    mysqli_close($db);
+  ?>
+
+  <form action="index.php">
+    <input type="submit" value="Back">
+  </form>
+</html>
