5cf22fabe7e7498ce44a8f748b852e9cda4e2ab6
Karthik Sudana <ksudana@wirelessprv-10-194-23-82.near.illinois.edu>
2017-11-16 01:52:34 -0600

    bjiofja

--- UserPortal/search.php
+++ UserPortal/search.php
@@ -37,8 +37,8 @@
 </footer>
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../Bootstrap/vendor/jquery/jquery.min.js"></script>
+<script src="../Bootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
