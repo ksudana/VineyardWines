814ec5dd1a63752bcdbbf2da771646819bd10bd0
Reddy <akhil3@illinois.edu>
2017-11-16 12:30:05 -0600

    just testing

--- UserPortal/editreview.php
+++ UserPortal/editreview.php
@@ -5,24 +5,6 @@
 <html>
   <?php
   $con = mysqli_connect($hn, $un, $pw, $db);
-  $sele = "SELECT * FROM Reviews WHERE uid='$uid'";
-  $result = mysql_query($sele);
-  if(!$result) {
-    print("Bad Query");
-  }
-  if(mysql_num_rows($result) > 0){
-    $i = 0;
-    while($row = mysql_fetch_assoc($result) and $i < 50){
-    echo "<table><tr<td><titlec><h3>". $row['title'] ."</h3></titlec></td></tr>";
-    echo "<tr><td><titlec> Rating:    ". $row['rating'] ."</titlec></td></tr>";
-    echo "<tr><td><titlec> Recommend: ". $row['recommend'] ."</titlec><br></td></tr>";
-    echo "<tr><td><titlec><h6>". $row['content'] ."</h6></titlec></td></tr>";
-    echo "<tr><td><titlec><h6> review id". $row['rid'] ."</h6></titlec></td></tr>";
-    echo "<tr><td><titlec> <p>Posted on   ". $row['date'] ."</p></titlec></td></tr></table>";
-    $rid = $row['rid'];
-    $content = $row['content'];
-    $i = $i + 1}
-  }
   if(!$con) {
     echo "Connection failed!";
   }
