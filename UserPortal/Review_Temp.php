3b348d9bf682457146ff498db02a87729b728f34
Akhil Reddy <sarikon2@illinois.edu>
2017-11-16 00:51:37 -0600

    edit

--- UserPortal/Review_Temp.php
+++ UserPortal/Review_Temp.php
@@ -96,7 +96,7 @@
                 echo "<tr><td><titlec> Comments:  ". $row['content'] ."</titlec></td></tr>";
                 echo "<tr><td><titlec> Rating:    ". $row['rating'] ."</titlec></td></tr>";
                 echo "<tr><td><titlec> Recommend: ". $row['recommend'] ."</titlec></td></tr>";
-                echo "<br><br><tr><td><titlec> Posted on". $row['date'] ."</titlec></td></tr></table><hr>";
+                echo "</br><tr><td><titlec> Posted on". $row['date'] ."</titlec></td></tr></table><hr>";
                 $i = $i + 1;
                 }
               }
