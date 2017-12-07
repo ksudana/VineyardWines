52729838d98f3b6e329b10d2f429bee5757bb650
Vineet Vora <Vineet@wirelessprv-10-194-170-61.near.illinois.edu>
2017-12-07 00:04:51 -0600

    fuck sudan

--- UserPortal/write.php
+++ UserPortal/write.php
@@ -10,7 +10,9 @@ if ($db->connect_errno) {
 
 
 //get userinput from url
-$username=substr($_GET["username"], 0, 32);
+
+$uid = $_SESSION['uid'];
+$username="SELECT username FROM Users WHERE uid='$uid'";
 $text=substr($_GET["text"], 0, 128);
 //escaping is extremely important to avoid injections!
 $nameEscaped = htmlentities(mysqli_real_escape_string($db,$username)); //escape username and limit it to 32 chars
