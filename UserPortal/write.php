fa85a45bf0c60002c39061caf30d52636a916c63
Vineet Vora <Vineet@wirelessprv-10-194-170-61.near.illinois.edu>
2017-12-07 00:21:29 -0600

    fuck sudan

--- UserPortal/write.php
+++ UserPortal/write.php
@@ -2,7 +2,7 @@
 require("./_connect.php");
 
 //connect to db
-$db = new mysqli($db_host,$db_user, $db_password, $db_name);
+$con = new mysqli($db_host,$db_user, $db_password, $db_name);
 if ($db->connect_errno) {
     //if the connection to the db failed
     echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
@@ -10,11 +10,15 @@ if ($db->connect_errno) {
 
 
 //get userinput from url
-
 $uid = $_SESSION['uid'];
-$use="SELECT username FROM Users WHERE uid='$uid'";
+$query = "SELECT * FROM Users WHERE uid='$uid'";
+$result = $con->query($query);
+$row = $result->fetch_assoc();
+if(!$row) {
+  echo "Invalid User!";
+}
 
-$username = mysql_query($sele);
+$username = $row['username'];
 
 $text=substr($_GET["text"], 0, 128);
 //escaping is extremely important to avoid injections!
