be87260ee91cb81c05013a1ae4fea5e6d6c530f7
Karthik Sudana <ksudana@wirelessprv-10-195-109-34.near.illinois.edu>
2017-12-06 22:17:45 -0600

    favs

--- UserPortal/favorite.php
+++ UserPortal/favorite.php
@@ -12,18 +12,17 @@
     $uid = $_SESSION['uid'];
     $wid = $_POST["wid"];
     $favorited = $_POST["in_favorites"];
-    else {
-        $query = "";
-        if($favorited){
-            $query = "DELETE FROM Favorites WHERE uid='$uid' AND wid='$wid'";
-        }
-        else {
-            $mysqldate = date( 'Y-m-d H:i:s');
-            $query = "INSERT INTO Favorites (uid, wid, date) VALUES ('$uid','$wid','$mysqldate')";
-        }
-        $result = mysqli_query($con, $query);
-        header("location: Review_Temp.php?wid=$wid");
+    $query = "";
+    if($favorited){
+        $query = "DELETE FROM Favorites WHERE uid='$uid' AND wid='$wid'";
     }
+    else {
+        $mysqldate = date( 'Y-m-d H:i:s');
+        $query = "INSERT INTO Favorites (uid, wid, date) VALUES ('$uid','$wid','$mysqldate')";
+    }
+    
+    $result = mysqli_query($con, $query);
+    header("location: Review_Temp.php?wid=$wid");
   }
     mysqli_close($db);
   ?>
