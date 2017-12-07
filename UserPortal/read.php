a4fedfe11f241cb2ab29bdd33dc813e2ab13c320
Akhil Reddy <sarikon2@illinois.edu>
2017-12-07 16:55:36 -0600

    lol

--- UserPortal/read.php
+++ UserPortal/read.php
@@ -10,7 +10,7 @@ if ($db->connect_errno) {
     echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 }
 
-$livetime = date('Y-m-d H:i:s', strtotime('-1 hour'));
+$livetime = date('g:i a', strtotime('-1 hour'));
 
 $query="SELECT * FROM chat ORDER BY id ASC";
 //execute query
