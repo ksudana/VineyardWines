edacf0b0c790c94ce7b1c92ccebcc4ca4d2f7c8b
Akhil Reddy <sarikon2@illinois.edu>
2017-12-07 14:29:40 -0600

    lol

--- UserPortal/read.php
+++ UserPortal/read.php
@@ -8,8 +8,8 @@ if ($db->connect_errno) {
     echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 }
 
-
-$query="SELECT * FROM chat WHERE time > ORDER BY id ASC";
+$now = new DateTime();
+$query="SELECT * FROM chat WHERE time > '$now' ORDER BY id ASC";
 //execute query
 if ($db->real_query($query)) {
 	//If the query was successful
