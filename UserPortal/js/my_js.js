59919a5178390778559ad3ae8b31a3b4f0b8228f
Akhil Reddy <sarikon2@illinois.edu>
2017-12-06 19:39:15 -0600

    lets try

--- UserPortal/js/my_js.js
+++ UserPortal/js/my_js.js
@@ -9,7 +9,6 @@ alert("Form Submitted Successfully...");
 }
 //Function To Display Popup
 function div_show() {
-    // document.getElementById('abc').style.display = "block";
 }
 //Function to Hide Popup
 function div_hide(){
