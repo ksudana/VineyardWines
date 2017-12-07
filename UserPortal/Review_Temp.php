aed950a4bb0040ae9541b4ba4be6ceca50cbf9a1
Akhil Reddy <sarikon2@illinois.edu>
2017-12-07 13:37:51 -0600

    lol

--- UserPortal/Review_Temp.php
+++ UserPortal/Review_Temp.php
@@ -217,7 +217,7 @@
 
     <!-- Bootstrap core JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="js/rChat.js"></script>
+    <script src="js/chat.js"></script>
     <script src="../Bootstrap/vendor/jquery/jquery.min.js"></script>
     <script src="../Bootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
