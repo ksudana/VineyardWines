bf37ed06716ed8d14b2c364f0a08c1430f3e3a39
Akhil Reddy <sarikon2@illinois.edu>
2017-12-07 23:13:28 -0600

    lol

--- UserPortal/read.php
+++ UserPortal/read.php
@@ -4,22 +4,17 @@ session_start();
 
 date_default_timezone_set('America/Chicago');
 
-
-//connect to db
 $uid = $_SESSION['uid'];
 
 $db = new mysqli($db_host,$db_user, $db_password, $db_name);
 if ($db->connect_errno) {
-	//if the connection to the db failed
     echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 }
 
 $livetime = date('g:i a', strtotime('-1 hour'));
 
 $query="SELECT * FROM chat ORDER BY id ASC";
-//execute query
 if ($db->real_query($query)) {
-	//If the query was successful
 	$res = $db->use_result();
 
     while ($row = $res->fetch_assoc()) {
@@ -31,7 +26,6 @@ if ($db->real_query($query)) {
         }
     }
 }else{
-	//If the query was NOT successful
 	echo "An error occured";
 	echo $db->errno;
 }
