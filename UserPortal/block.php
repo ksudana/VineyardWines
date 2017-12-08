d0cd44b924bc44589f5428905f19454c26c07f18
Vineet Vora <Vineet@wirelessprv-10-194-170-61.near.illinois.edu>
2017-12-08 01:12:55 -0600

    fuck sudan

--- UserPortal/block.php
+++ UserPortal/block.php
@@ -4,31 +4,28 @@
 ?>
 <html>
   <?php
-  $con = mysqli_connect($hn, $un, $pw, $db);
-  if(!$con) {
-    echo "Connection failed!";
-  }
+  $mysqli = new mysqli(($hn, $un, $pw, $db);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
   else {
+    $mysqli->autocommit(FALSE);
+
     $my_uid = $_SESSION['uid'];
     $foreign_uid = $_POST["foreign_uid"];
-    try {
-    // First of all, let's begin a transaction
-    $db->beginTransaction();
+    mysqli_query("START TRANSACTION");
 
-    // A set of queries; if one fails, an exception should be thrown
-    $db->query('DELETE FROM Follows WHERE uid1='$my_uid' AND uid2='$foreign_uid'');
-    $db->query('DELETE FROM Follows WHERE uid2='$my_uid' AND uid1='$foreign_uid'');
-    // If we arrive here, it means that no exception was thrown
-    // i.e. no query has failed, and we can commit the transaction
-    $db->commit();
-} catch (Exception $e) {
-    // An exception has been thrown
-    // We must rollback the transaction
-    $db->rollback();
-}
+    $a1 = $mysqli->query("DELETE FROM Follows WHERE uid1='$my_uid' AND uid2='$foreign_uid'");
+    $a2 = $mysqli->query("DELETE FROM Follows WHERE uid2='$my_uid' AND uid1='$foreign_uid'");
 
+  if ($a1 and $a2) {
+      $mysqli->commit();
+  } else {        
+      $mysqli->rollback();
+  }
   header("location: otherusers.php?otherid=$foreign_uid");
 }
-    mysqli_close($db);
+    $mysqli->close;
   ?>
 </html>
