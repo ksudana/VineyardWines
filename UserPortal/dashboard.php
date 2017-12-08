38a503da381319b540b5e0923f1ea3a0db9c7ea5
Reddy <akhil3@illinois.edu>
2017-12-08 01:08:18 -0600

    lol

--- UserPortal/dashboard.php
+++ UserPortal/dashboard.php
@@ -136,37 +136,27 @@
               echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
             }
 
-if (!$mysqli->query("DROP TABLE IF EXISTS test") ||
-    !$mysqli->query("CREATE TABLE test(id INT)") ||
-    !$mysqli->query("INSERT INTO test(id) VALUES (1), (2), (3)")) {
-    echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
+            if (!$mysqli->query("DROP PROCEDURE IF EXISTS p") ||
+              !$mysqli->query("CREATE PROCEDURE p() READS SQL DATA BEGIN SELECT * FROM Favorites WHERE uid='$uid'; END;")) {
+              echo "Stored procedure creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            }
 
-if (!$mysqli->query("DROP PROCEDURE IF EXISTS p") ||
-    !$mysqli->query('CREATE PROCEDURE p() READS SQL DATA BEGIN SELECT id FROM test; SELECT id + 1 FROM test; END;')) {
-    echo "Stored procedure creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-
-if (!($stmt = $mysqli->prepare("CALL p()"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-}
-
-do {
-    if ($res = $stmt->get_result()) {
-        printf("---\n");
-        var_dump(mysqli_fetch_all($res));
-        mysqli_free_result($res);
-    } else {
-        if ($stmt->errno) {
-            echo "Store failed: (" . $stmt->errno . ") " . $stmt->error;
-        }
-    }
-} while ($stmt->more_results() && $stmt->next_result());
-?>
+            if (!$mysqli->query("CALL p()")) {
+              echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            }
+            printf("fuck this bullshit as shit");
+            do {
+                if ($res = $mysqli->store_result()) {
+                    printf("---\n");
+                    var_dump($res->fetch_all());
+                    $res->free();
+                } else {
+                    if ($mysqli->errno) {
+                        echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
+                    }
+                }
+            } while ($mysqli->more_results() && $mysqli->next_result());
+            ?>
 
 
 
